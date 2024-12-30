6038bfdb85ad06340b45d36d748b86771fdf8037
Chamali Vishmani <chamali106astro@gmail.com>
2024-12-30 23:51:34 +0530

    restructure for courses

---
+++ DSA/sorting_and_searching/bogo_sort.py
@@ -0,0 +1,11 @@
+"""
+randomly generating different permutations of an array
+and then checking whether it is sorted or not
+"""
+import sys
+
+from load import load_numbers
+
+numbers = load_numbers(sys.argv[1])
+
+print(numbers)
